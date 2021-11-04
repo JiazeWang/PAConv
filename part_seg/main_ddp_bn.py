66cc01275511a2ec7d63b8a5671f4ea14e0579cc
jiazewang <jzwang@link.cuhkedu.hk>
2021-11-04 21:47:58 +0800

    5

--- part_seg/main_ddp_bn.py
+++ part_seg/main_ddp_bn.py
@@ -95,7 +95,7 @@ def train(gpu, ngpus_per_node):
     # ============= Model ===================
     num_part = 50
     from model.model_gcn3d_bn import GCN3D
-    model = model = GCN3D(class_num= 50, support_num= 1, neighbor_num= 100)
+    model = model = GCN3D(class_num= 50, support_num= 1, neighbor_num= 5)
 
     model.apply(weight_init)
 
